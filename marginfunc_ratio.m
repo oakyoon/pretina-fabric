6518bc0bf496d24b151b11ccbf94fc66e21d2fc5
Oakyoon Cha <oakyoon@gmail.com>
2018-04-04 09:50:05 -0500

    updated to work with one-parameter shapes

--- marginfunc_ratio.m
+++ marginfunc_ratio.m
@@ -3,6 +3,10 @@ function shape = marginfunc_ratio(shape, margin)
 		margin = 1;
 	end
 	pr1 = shape.params(1) + margin;
-	pr2 = shape.params(1) / shape.params(2) + margin;
-	shape.params(1:2) = [pr1, pr1 / pr2];
+	if size(shape.params, 2) >= 2
+		pr2 = shape.params(1) / shape.params(2) + margin;
+		shape.params(1:2) = [pr1, pr1 / pr2];
+	else
+		shape.params(1) = pr1;
+	end
 end
